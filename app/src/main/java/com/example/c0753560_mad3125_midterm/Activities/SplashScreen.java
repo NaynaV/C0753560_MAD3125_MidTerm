1e26fe9abc0f66989b251480bfa92489be7275ca
NaynaV <nainavaghasiya9@gmail.com>
2019-08-08 19:09:34 -0400

    [Nayna] Json file added with data

--- app/src/main/java/com/example/c0753560_mad3125_midterm/Activities/SplashScreen.java
+++ app/src/main/java/com/example/c0753560_mad3125_midterm/Activities/SplashScreen.java
@@ -1,7 +1,38 @@
 package com.example.c0753560_mad3125_midterm.Activities;
 
-public class SplashScreen
+import android.content.Intent;
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.c0753560_mad3125_midterm.R;
+
+public class SplashScreen extends AppCompatActivity
 {
 
 
+
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splashscreen);
+        Thread myThread = new Thread(){
+            @Override
+            public void run() {
+                try {
+                    sleep(3000);
+                    Intent intent = new Intent(getApplicationContext(), LoginClass.class);
+                    startActivity(intent);
+                    finish();
+                }
+                catch
+                (InterruptedException e){
+                    e.printStackTrace();
+                }
+            }
+        };
+        myThread.start();
+    }
 }
