a8efafd04382b9bd177485b11b4f654a40d2fe9c
NaynaV <nainavaghasiya9@gmail.com>
2019-08-08 21:31:26 -0400

    [Nayna]  json working

--- app/src/main/java/com/example/c0753560_mad3125_midterm/MainActivity.java
+++ app/src/main/java/com/example/c0753560_mad3125_midterm/MainActivity.java
@@ -4,8 +4,11 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.content.Context;
 import android.os.Bundle;
 import android.widget.LinearLayout;
+
+import java.io.InputStream;
 import java.util.ArrayList;
 
 import com.android.volley.Request;
@@ -21,11 +24,30 @@ import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.nio.charset.StandardCharsets;
+
 public class MainActivity extends AppCompatActivity {
 
     private FlightListAdaptor mflightAdapter;
     private ArrayList<FlightMain> mflightList;
     private RequestQueue mRequestQueue;
+
+
+    Context context;
+
+
+    public ArrayList<FlightMain> getmSpaceXFlightList() {
+        return mflightList;
+    }
+
+    public MainActivity(Context context)
+    {
+        this.context = context;
+    }
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -75,5 +97,108 @@ public class MainActivity extends AppCompatActivity {
 
         mRequestQueue.add(request);
     }
+
+    public String loadJSONFromAsset() {
+        String json;
+        try {
+            InputStream is = context.getAssets().open("Flight.json");
+            int size = is.available();
+            byte[] buffer = new byte[size];
+            int count = is.read(buffer);
+            is.close();
+            //Log.d("-- COUNT --", String.format("%d Bytes",count));
+            json = new String(buffer, "UTF8");
+        } catch (IOException ex) {
+            ex.printStackTrace();
+            return null;
+        }
+        return json;
     }
+
+    public void processJSON()
+    {
+        String jsonString = this.loadJSONFromAsset();
+        if (jsonString != null)
+        {
+            try {
+                JSONArray mJSONArray = new JSONArray(jsonString);
+                mflightList = new ArrayList<FlightMain>();
+
+                for (int i = 0; i < mJSONArray.length(); i++) {
+
+                    FlightMain mFlightMain = getFlightObjectFromJSON(mJSONArray.getJSONObject(i));
+                    mflightList.add(mFlightMain);
+                    //Log.d("-- JSON -- ", mSpaceXFlight.toString());
+
+                }
+            }
+            catch(JSONException e)
+            {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    public FlightListAdaptor getFlightObjectFromJSON(JSONObject userJsonObject) throws JSONException
+    {
+        String flight_number = userJsonObject.getString("flight_number");
+        String mission_name = userJsonObject.getString("mission_name");
+        String upcoming = userJsonObject.getString("upcoming");
+        String launch_year = userJsonObject.getString("launch_year");
+        String launch_window = userJsonObject.getString("launch_window");
+        String details = userJsonObject.getString("details");
+
+        //Read Rocket
+        JSONObject rocket = new JSONObject(userJsonObject.getJSONObject("rocket").toString());
+        String rocket_id = rocket.getString("rocket_id");
+        String rocket_name = rocket.getString("rocket_name");
+        String rocket_type = rocket.getString("rocket_type");
+
+        //Read Launch Site
+        JSONObject launchSite = new JSONObject(userJsonObject.getJSONObject("launch_site").toString());
+        String site_id = launchSite.getString("site_id");
+        String site_name = launchSite.getString("site_name");
+        String site_name_long = launchSite.getString("site_name_long");
+
+        //Read Links
+        JSONObject links = new JSONObject(userJsonObject.getJSONObject("links").toString());
+        String mission_patch = links.getString("mission_patch");
+        String mission_patch_small = links.getString("mission_patch_small");
+        String article_link = links.getString("article_link");
+        String wikipedia = links.getString("wikipedia");
+        String video_link = links.getString("video_link");
+
+        //start creating User object
+        SpaceXFlight mSpaceXFlight = new SpaceXFlight();
+        mSpaceXFlight.setFlight_number(flight_number);
+        mSpaceXFlight.setMission_name(mission_name);
+        mSpaceXFlight.setUpcoming(upcoming);
+        mSpaceXFlight.setLaunch_year(launch_year);
+        mSpaceXFlight.setLaunch_window(launch_window);
+        mSpaceXFlight.setDetails(details);
+
+        Rocket mRocket = new Rocket();
+        mRocket.setRocket_id(rocket_id);
+        mRocket.setRocket_name(rocket_name);
+        mRocket.setRocket_type(rocket_type);
+        mSpaceXFlight.setRocket(mRocket);
+
+        LaunchSite mLaunchSite = new LaunchSite();
+        mLaunchSite.setSite_id(site_id);
+        mLaunchSite.setSite_name(site_name);
+        mLaunchSite.setSite_name_long(site_name_long);
+        mSpaceXFlight.setLaunchSite(mLaunchSite);
+
+        Links mLinks = new Links();
+        mLinks.setMission_patch(mission_patch);
+        mLinks.setMission_patch_small(mission_patch_small);
+        mLinks.setArticle_link(article_link);
+        mLinks.setWikipedia(wikipedia);
+        mLinks.setVideo_link(video_link);
+        mSpaceXFlight.setLinks(mLinks);
+
+        return mSpaceXFlight;
+    }
+
+
 }
