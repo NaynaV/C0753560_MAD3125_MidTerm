16f6054c0d59889e1539dda284021e523fed6006
NaynaV <nainavaghasiya9@gmail.com>
2019-08-08 21:04:51 -0400

    [Nayna]  working on json code to get data

--- app/src/main/java/com/example/c0753560_mad3125_midterm/MainActivity.java
+++ app/src/main/java/com/example/c0753560_mad3125_midterm/MainActivity.java
@@ -1,14 +1,79 @@
 package com.example.c0753560_mad3125_midterm;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+import android.widget.LinearLayout;
+import java.util.ArrayList;
+
+import com.android.volley.Request;
+
+import com.android.volley.RequestQueue;
+
+import com.android.volley.Response;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
+import com.example.c0753560_mad3125_midterm.JavaClass.FlightMain;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 
 public class MainActivity extends AppCompatActivity {
 
+    private FlightListAdaptor mflightAdapter;
+    private ArrayList<FlightMain> mflightList;
+    private RequestQueue mRequestQueue;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        RecyclerView FlightListRecycleView = findViewById(R.id.flightListRecyclerView);
+        FlightListRecycleView.setLayoutManager(new LinearLayoutManager(this));
+
+        mRequestQueue = Volley.newRequestQueue(this);
+        parseJSON();
+    }
+
+    public void parseJSON()
+    {
+        String url = "https://pixabay.com/api/?key=5303976-fd6581ad4ac165d1b75cc15b3&q=kitten&image_type=photo&pretty=true";
+
+        JsonObjectRequest request = new JsonObjectRequest(Request.Method.GET, url, null,
+                new Response.Listener<JSONObject>() {
+                    @Override
+                    public void onResponse(JSONObject response) {
+                        try {
+                            JSONArray jsonArray = response.getJSONArray("hits");
+
+                            for (int i = 0; i < jsonArray.length(); i++) {
+                                JSONObject hit = jsonArray.getJSONObject(i);
+
+                                String flightName = hit.getString("mission_name");
+                                String flightImage = hit.getString("mission_patch_small");
+                                String launchYear = hit.getString("launch_year");
+
+                                mflightList.add(new FlightMain(flightName, flightImage, launchYear));
+                            }
+
+                            mflightAdapter = new FlightListAdaptor(MainActivity.this, mflightList);
+                            mRecyclerView.setAdapter(mExampleAdapter);
+
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                error.printStackTrace();
+            }
+        });
+
+        mRequestQueue.add(request);
+    }
     }
 }
